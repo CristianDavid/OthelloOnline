5ef6f24147e0f7b6a1b22f5ea5882b582875968b
oscar <oscar.adrian.mariscal@Gmail.com>
2015-11-13 20:52:17 -0600

    Iniciando la clase de partida

---
+++ ClasePartida/main.cpp
@@ -0,0 +1,12 @@
+#include "mainwindow.h"
+#include <QApplication>
+#include <iostream>
+#include "partida.h"
+
+using namespace std;
+int main(int argc, char *argv[])
+{
+    Partida a;
+
+    return 0;
+}
