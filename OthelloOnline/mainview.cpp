14eaabe78a46f2f847c386fc3f1c757dff89aebd
Cristian David Gonzalez Gonzalez <cristian_david_g@live.com>
2015-11-20 15:25:19 -0600

    mas avances

--- OthelloOnline/mainview.cpp
+++ OthelloOnline/mainview.cpp
@@ -2,15 +2,25 @@
 #include <QGraphicsScene>
 #include <QGraphicsSimpleTextItem>
 #include <QFontDatabase>
+#include <QFont>
+#include <QDebug>
+#include <cstdio>
+#include <cstdlib>
+
+#define DEFAULT_FONT "fonts/d-puntillas-D-to-tiptoe.ttf"
 
 MainView::MainView() {
    QGraphicsScene *menu = new QGraphicsScene();
    QGraphicsSimpleTextItem *text = new QGraphicsSimpleTextItem("Jugar");
-   int id = QFontDatabase::addApplicationFont("fonts/");
+   int fontId = QFontDatabase::addApplicationFont(DEFAULT_FONT);
+   if (fontId != -1) {
+      QString family = QFontDatabase::applicationFontFamilies(fontId).at(0);
+      QFont font(family, 40, 40, true);
+      text->setFont(font);
+   }
    setFixedSize(800, 600);
    menu->addItem(text);
    menu->setSceneRect(0, 0, 790, 590);
    text->setPos(menu->width() / 2, 0);
-   text->setFont(font);
    setScene(menu);
 }
