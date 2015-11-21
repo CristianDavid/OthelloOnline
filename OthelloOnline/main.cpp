f86d73b4cc719d43143943eb37c71642ea5368d6
Cristian David Gonzalez Gonzalez <cristian_david_g@live.com>
2015-11-20 21:46:07 -0600

    Reproduce música de fondo

--- OthelloOnline/main.cpp
+++ OthelloOnline/main.cpp
@@ -1,11 +1,17 @@
 #include <QApplication>
 #include <QGraphicsRectItem>
+#include <QMediaPlayer>
 #include "mainview.h"
 
 int main(int argc, char *argv[]) {
    QApplication app(argc, argv);
    MainView mainView;
    mainView.show();
+
+   QMediaPlayer *player = new QMediaPlayer;
+   player->setMedia(QUrl::fromLocalFile("/home/cristian/Source/PrograInternet/ProyectoFinal/OthelloOnline/build-debug/audio/OthelloOnline.wav"));
+   player->setVolume(50);
+   player->play();
 
    return app.exec();
 }
