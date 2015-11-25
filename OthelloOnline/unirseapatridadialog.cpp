c8ae77e5a864fa3628d189d76f0dccd01533f977
Cristian David Gonzalez Gonzalez <cristian_david_g@live.com>
2015-11-24 18:00:05 -0600

    dialogo mejorado

--- OthelloOnline/unirseapatridadialog.cpp
+++ OthelloOnline/unirseapatridadialog.cpp
@@ -1,22 +1,42 @@
 #include "unirseapatridadialog.h"
 #include "ui_unirseapatridadialog.h"
+#include <cstring>
+#include <sys/types.h>
 #include <QPushButton>
+#include <QLineEdit>
 #include <QListWidget>
 #include <QListWidgetItem>
 #include <QString>
 #include <QDebug>
 
+class HostWidgetItem : QListWidgetItem {
+ public:
+   HostWidgetItem(const char *hostName, const char *nombrePartida) {
+      strncpy(this->hostName, (host != NULL)? hostName : "", 255);
+      strncpy(this->nombrePartida, (nombrePartida != NULL)? nombrePartida : "", 255);
+      setText(QString(getNombrePartida()) + " - " + getHostName());
+   }
+   const char *getHostName();
+   const char *getNombrePartida();
+ private:
+   char hostName[256];
+   char nombrePartida[256];
+};
+
 UnirseAPatridaDialog::UnirseAPatridaDialog(QWidget *parent) :
    QDialog(parent),
    ui(new Ui::UnirseAPatridaDialog) {
    ui->setupUi(this);
-   const QString childName("listWidget");
-   QListWidget *partidas = findChild<QListWidget*>(childName);
-   for (int i = 0; i < 20; i++) {
-      partidas->addItem("cosa");
-   }
+   QListWidget *partidas = findChild<QListWidget*>("listWidget");
+   HostWidgetItem *partidaLocal = new HostWidgetItem("localhost", "cristian");
+   partidas->addItem(partidaLocal);
 }
 
 UnirseAPatridaDialog::~UnirseAPatridaDialog() {
    delete ui;
+}
+
+void UnirseAPatridaDialog::on_UnirseAPatridaDialog_accepted() {
+   QLineEdit *selectedHost = findChild<QLineEdit*>("selectedHost");
+   qDebug() << selectedHost->text();
 }
