10101ead6be3eb6ad866ae615374e31440faa770
oscarAdrianMariscal <oscar.adrian.mariscal@gmail.com>
2015-11-25 00:16:55 -0600

    agregado un poco de avanzes sobre la clase partida, acutalmente tiene un bug.

--- OthelloOnline/partida.cpp
+++ OthelloOnline/partida.cpp
@@ -17,17 +17,31 @@ Partida::Partida(){
     tablero[ANCHO_TABLERO/2-1][ANCHO_TABLERO/2]=JUGADOR_NEGRO;
 }
 
-bool Partida::esMovimientoValido(int x, int y){
-    bool movimientoValido;
+bool Partida::esMovimientoValido(int x, int y, char jugador){
+    bool movimientoValido=false;
+    std::vector<int> vector;
     if (tablero[x][y]==LUGAR_VACIO){
-
+    	/*Buscando en todas las direcciones desde las cordenadas (x,y) para ver
+			si hay algún movimiento valido*/
+    	for (int i = 0; i <= SUROESTE; ++i)	{
+    		vector=posicionesACambiar(jugador,y,obtenerLinea(x,y,i));
+    		if (vector.empty()){
+    			movimientoValido=true;
+    			break;
+    		}
+    	}
     }
-    else movimientoValido=false;
     return movimientoValido;
 }
 
 void Partida::imprimirTablero(){
+	cout << " ";
+	for (int i = 0; i < ANCHO_TABLERO; ++i) {
+		cout << i << "|";
+	}
+	cout << endl;
     for (int i = 0; i < ANCHO_TABLERO; ++i) {
+    	cout << i;
         for (int j = 0; j < ALTO_TABLERO; ++j) {
             cout << tablero[i][j]<< "|";
         }
@@ -35,6 +49,7 @@ void Partida::imprimirTablero(){
     }
 }
 
+//Te devuelve una linea del tablero dependiedo de la direccion 
 string Partida::obtenerLinea(int x , int y, int direccion){
 	//Tal vez deberia separar este metodo.
 	char cadena[ALTO_TABLERO+1];
@@ -78,7 +93,8 @@ string Partida::obtenerLinea(int x , int y, int direccion){
 	return lineaDeseada;
 }
 
-bool Partida::esLaLineaValida(char jugador,int y, std::string linea){
+//Te devuelve las posiciones en una linea que cambiarian.
+vector<int> Partida::posicionesACambiar(char jugador,int y, std::string linea){
 	bool esValida;
 	char contrario;
 	contrario=(jugador==JUGADOR_NEGRO)?JUGADOR_BLANCO:JUGADOR_NEGRO;
@@ -86,6 +102,9 @@ bool Partida::esLaLineaValida(char jugador,int y, std::string linea){
 	string parteUno,parteDos;
 	parteUno = v.at(0);
 	parteDos = v.at(1);
+	std::vector<int> posiciones;
+	std::vector<int> temp;
+
 
 	if (y+1>linea.length()){
 		cout << "ERROR: NO PUEDES PASAR UN X MAYOR QUE LA CADENA" << endl;
@@ -93,57 +112,80 @@ bool Partida::esLaLineaValida(char jugador,int y, std::string linea){
 	else{
 		if (linea.length()>=3){
 			//Primera cadena.
-			cout << "Primera cadena :" << parteUno << "|";
 			int j=(parteUno.length()-1)-1;
 			if (parteUno[j]==contrario){
 				while(j>0 && parteUno[j]==contrario ){
-					cout << j<< "*j*" <<endl;
+					temp.push_back(j);
 					j--;
 				}
 				if(j>=0 && parteUno[j]==jugador ){
-					esValida=true;
-					cout << "true";
+					posiciones.push_back(y);
+					for (int k =0 ; k < temp.size();k++){
+						posiciones.push_back(temp.at(k));
+					}
 				}
 
 			}
-			else{
-				esValida=false;
-			}
-
-
 			//Segunda cadena
-			cout << "Segunda cadena:" << parteDos << "|";
-			if(esValida==false){
-				int i=1;
-				if (parteDos[i]==contrario){
-					while(i<parteDos.length() && parteDos[i]==contrario ){
-						i++;
-						cout << i+y<< "*i*" <<endl;
+			temp.clear();
+			int i=1;
+			if (parteDos[i]==contrario){
+				while(i<parteDos.length() && parteDos[i]==contrario ){
+					i++;
+					temp.push_back(i+y-1);
+					temp.push_back(y);
+				}
+				if(i<parteDos.length() && parteDos[i]==jugador ){
+					for (int k =0 ; k < temp.size();k++){
+						posiciones.push_back(temp.at(k));
 					}
-					if(i<parteDos.length() && parteDos[i]==jugador ){
-						esValida=true;
-						cout << "true";
-					}
+				}
 
-				}
-				else{
-					esValida=false;
-				}
-				
 			}
-
 		}
-		else 
-			esValida=false;
 	}
-	return esValida;
+	return posiciones;
 }
 
 void Partida::hacerMovimiento(int x, int y , char jugador){
-	tablero[x][y]=jugador;
+	if (esMovimientoValido(x,y,jugador)){
+		//Horizontal
+		std::vector<int> v;
+		v= posicionesACambiar(jugador,y,obtenerLinea(x,y,HORIZONTAL));
+		for (int i =0 ; i< v.size(); i++){
+			tablero[x][v.at(i)]=JUGADOR_BLANCO;
+		}
+		v.clear();
+		//Vertical
+		v= posicionesACambiar(jugador,x,obtenerLinea(x,y,VERTICAL));
+		for (int i =0 ; i< v.size(); i++){
+			tablero[v.at(i)][y]=JUGADOR_BLANCO;
+		}
+		v.clear();
+		//NOROESTE
+		v= posicionesACambiar(jugador,x,obtenerLinea(x,y,NOROESTE));
+		int c,d,i;
+		c=x;
+		d=y;
+		while ((c!=0) && (d!=0) ){
+			d--;
+			c--;
+		}
+		sort(v.begin(),v.end());
+		v.erase( unique( v.begin(), v.end() ), v.end() );
+		for (i=0; c<ANCHO_TABLERO && d < ALTO_TABLERO; c++,d++,i++){
+			if (find(v.begin(), v.end(), i) != v.end()){
+				tablero[x][y]=jugador;
+			}
+		}
+	}
+	else{
+		cout << "NO ES MOVIMIENTO VALIDO"<< endl;
+	}
 }
 
-
+/*Metodo de utileria para dividir la cadena en dos, las dos cadenas
+ incluyen el char en "posicion"*/
 vector<string> Partida::split(string s, int posicion){
 	// =(
 	std::vector<string> v;
@@ -162,4 +204,8 @@ vector<string> Partida::split(string s, int posicion){
 	v.push_back(cadenaUno);
 	v.push_back(cadenaDos);
 	return v;
+}
+//Solo para hacer pruebas mientra creo hacer movimiento
+void Partida::mierda(int x, int y , char jugador){
+	tablero[x][y]=jugador;
 }
--- OthelloOnline/partida.h
+++ OthelloOnline/partida.h
@@ -18,22 +18,26 @@ se crea dede sureste hacia noreste*/
 #include <string>
 #include <string.h>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 class Partida{
+public:
     char tablero[ANCHO_TABLERO][ALTO_TABLERO];
     //jugadorRemoto
     //JugadorLocal
     //para pruebas.
 private:
-	std::vector<std::string> split(string s, int posicion);
+	vector<string> split(string s, int posicion);
 public:
     void imprimirTablero();
-    std::string obtenerLinea(int x, int y , int direccion);
-    bool esLaLineaValida(char jugador,int y, std::string linea);
+    string obtenerLinea(int x, int y , int direccion);
+    vector<int> posicionesACambiar(char jugador,int y,string linea);
     void hacerMovimiento(int x, int y , char jugador);
     Partida();
-    bool esMovimientoValido(int x, int y);
+    bool esMovimientoValido(int x, int y, char jugador);
+    //Solo para hacer pruebas mientra hago hacerMoviemtno
+    void mierda(int x, int y , char jugador);
 
 };
 
